b756c7a72cc401040bc3094ee9bc2138b3a9f5c7
TuXsOr <55958104+TuXsOr@users.noreply.github.com>
2023-10-30 13:29:09 -0500

    Server Project Setup
    Basic Server Project setup with few classes added. (Committed to move to laptop)

---
+++ Server/Classes/GlobalManager.cs
@@ -0,0 +1,25 @@
+﻿namespace Server.Classes
+{
+    internal class GlobalManager
+    {
+        private bool running = false;
+
+        // Manager declerations
+        internal FileManager fileManager;
+        internal NetworkManager networkManager;
+
+        // During Construction create and store references to other needed manager classes
+        public GlobalManager()
+        {
+            // instanstiate the other managers
+            fileManager = new FileManager();
+            networkManager = new NetworkManager();
+        }
+
+        public void StartServer()
+        {
+            running = true;
+            Console.WriteLine("Starting server");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using Server.Classes;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            GlobalManager manager = new GlobalManager();
+
+            manager.StartServer();
+        }
+    }
+}
